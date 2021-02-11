849c024115c7e07937235c1e55949e3605baa3e8
Thom <ste17059@byui.edu>
2021-02-11 08:01:06 -0700

    added timer class

---
+++ timer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import time
+import sys
+
+#create timer
+class timer:
+    #initialize timer
+    def __init__(self):
+        time_start = time.time()
+        seconds = 0
+        minutes = 0
+        lava_level = 1
+
+    def run(self):
+        while True:
+            sys.stdout.write("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds))
+            sys.stdout.flush()
+            time.sleep(1)
+            seconds = int(time.time() - time_start) - minutes * 60
+            if seconds >= 60:
+                minutes += 1
+                seconds = 0
+            if seconds % 10 == 0:
+                lava_level += 1
+          #  print(lava_level)
+
