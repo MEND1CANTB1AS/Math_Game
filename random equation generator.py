ca22401c9465f4e0f0e0ceb7e60d81ec54425fcc
Abigail Priebe <74333794+abipriebe@users.noreply.github.com>
2021-03-02 08:27:22 -0700

    Add files via upload

--- random equation generator.py
+++ random equation generator.py
@@ -1,45 +1,49 @@
+import arcade
 import random
 
-def math():
-    # the operator (+, -, *, /) will be randomly assigned
-    operator = random.randint(1, 4)
+    def equation(self):
+        # the operator (+, -, *, /) will be randomly assigned
+        operator = random.randint(1, 4)
 
-    # addition problems
-    if operator == 1:
-        x = int(random.randint(0, 10))
-        y = int(random.randint(0, 10))
-        question = int(input(f'{x} + {y} = '))
-        answer = x + y
-        if question == answer:
-            print("Correct!")
+        # addition problems
+        if operator == 1:
+            x = int(random.randint(0, 10))
+            y = int(random.randint(0, 10))
+            question = int(input(f'{x} + {y} = '))
+            answer = x + y
+            if question == answer:
+                print("Correct!")
 
-    # subtraction problems
-    elif operator == 2:
-        x = int(random.randint(0, 10))
-        y = int(random.randint(0, 10))
-        question = int(input(f'{x} - {y} = '))
-        answer = x - y
-        if question == answer:
-            print('Correct!')
+        # subtraction problems
+        elif operator == 2:
+            x = int(random.randint(0, 10))
+            y = int(random.randint(0, x))
+            question = int(input(f'{x} - {y} = '))
+            answer = x - y 
+            if question == answer:
+                print('Correct!')
 
-    # multiplication problems
-    elif operator == 3:
-        x = int(random.randint(0, 10))
-        y = int(random.randint(0, 10))
-        question = int(input(f'{x} x {y} = '))
-        answer = x * y
-        if question == answer:
-            print('Correct!')
+        # multiplication problems
+        elif operator == 3:
+            x = int(random.randint(0, 11))
+            y = int(random.randint(0, 9))
+            question = int(input(f'{x} x {y} = '))
+            answer = x * y
+            if question == answer:
+                print('Correct!')
 
-    # division problems
-    elif operator == 4:
-        x = int(random.randint(1, 10))
-        y = int(random.randint(0, 10))
-        z = x * y
-        question = int(input(f'{z} / {x} = '))
-        answer = z / x
-        if question == answer:
-            print('Correct!')
+        # division problems
+        else:
+            x = int(random.randint(1, 10))
+            y = int(random.randint(0, 10))
+            z = x * y
+            question = int(input(f'{z} / {x} = '))
+            answer = z / x
+            if question == answer:
+                print('Correct!')
 
-for i in range(10):
-    math()
+    def draw_question(self):
+        equation_text = equation.question
+        start_x = 10
+        start_y = SCREEN_HEIGHT - 20
+        arcade.draw_text(equation_text, start_x=start_x, start_y=start_y, font_size=12, color=arcade.color.NAVY_BLUE)
